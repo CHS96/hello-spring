17c9dbdb0565a289480c61dbdae29451695f3056
CHS96 <sorjfkrh5078@gmail.com>
2021-01-19 12:51:40 +0900

    jdbc 사용해보기, h2데이터베이스와 연동 문제 발생

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,14 +1,23 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 //컴포넌트 스캔말고 직접 자바 코드로 스프링에 등록
 //다만 Controller는 안됨
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -20,6 +29,7 @@ public class SpringConfig {
     //후에 어떤 DB를 쓰랴고 DBmemberRepository 바꿀때 코드 수정없이 여기서 return new DBmemberRepository로 바꿔주기만 하면 됨
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
